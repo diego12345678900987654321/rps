db57532c26594550cc51c4d295fe26f5ddc0fe73
diego12345678900987654321 <drodriguez1025@icloud.com>
2014-04-11 17:37:32 +0000

    added hashtags to check rock paper scissors

--- simple_rps.rb
+++ simple_rps.rb
@@ -1,4 +1,9 @@
 OUTCOMES = {"rock" => {"rock" => "tie", "paper" => "player 2", "scissors" => "player 1"},
-            "paper" =>{},
-            "scissors" =>{}
+            "paper" =>{"paper" => "tie", "rock" => "player1", "scissors" => "player2"},
+            "scissors" =>{"scissors" => "tie", "rock" => "player2", "paper" => "player1"}
             }
+
+computer_choice = ["rock", "paper", "scissors"].sample
+puts "Pick a choice: rock, paper, scissors"
+user_choice = gets.chomp
+puts OUTCOMES[user_choice][computer_choice]
